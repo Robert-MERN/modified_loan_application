4290f4b1dca016780eecc750e24ad925f24c214b
Muneeb Ahmed MERN <94203826+Robert-MERN@users.noreply.github.com>
2025-10-13 18:23:15 +0500

    Update receipt-generator.jsx

--- loan/pages/receipt-generator.jsx
+++ loan/pages/receipt-generator.jsx
@@ -1,6 +1,21 @@
 import Receipt_generator from '@/components/Receipt_generator'
 import React from 'react'
 
+function cleanObject(obj) {
+    const cleanedObj = {};
+    for (const key in obj) {
+        if (obj[key] === undefined) {
+            cleanedObj[key] = null;
+        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+            cleanedObj[key] = cleanObject(obj[key]); // Recursively clean nested objects
+        } else {
+            cleanedObj[key] = obj[key];
+        }
+    }
+    return cleanedObj;
+}
+
+
 const receipt_generator = () => {
     return (
         <main className="min-h-screen flex items-center justify-center bg-gray-50">
